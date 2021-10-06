b3b19c65637a225f0e54f252864285b788413d94
Thomas Rao <thomas.rao@outlook.com>
2021-10-06 13:18:26 -0400

    Fixed virtual methods, added Card::cancel(), added TODOs for future reference, added comments.

--- include/Orders.h
+++ include/Orders.h
@@ -3,57 +3,94 @@
 #include <iostream>
 
 class Order {
+    protected:
+    bool _cancelled;
+    // TODO: might need team reference here.
     public:
-    
-    virtual void execute();
-    virtual bool validate();
+    bool cancel();
+    virtual void execute()=0;
+    virtual bool onCancel()=0;
+    virtual bool validate()=0;
     friend std::ostream& operator<<(std::ostream &out, const Order &order);
 };
 
-class Deploy : public Order {
+/**
+ * Order caused by using a card.
+ **/
+class CardOrder : public Order {
+    protected:
+    // TODO: add Player* and/or Card* as class reference.
     public:
-    Deploy();
+    CardOrder();
+    ~CardOrder();
+    virtual void execute()=0;
+    bool onCancel();
+    virtual bool validate()=0;
+};
+
+class Deploy : public Order {
+    private:
+    int _units;
+    // TODO: add territory as class field.
+    public:
+    Deploy(int units); // Add territory to constructor.
     ~Deploy();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool onCancel();
+    bool validate();
 };
 
 class Advance : public Order {
+    private:
+    int _units;
+    // TODO: add territory (from) as class field.
+    // TODO: add territory (to) as class field.
     public:
-    Advance();
+    Advance(int units); // Add territories to constructor.
     ~Advance();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool onCancel();
+    bool validate();
 };
 
-class Bomb : public Order {
+/**
+ * Order caused by using a card.
+ **/
+class Bomb : public CardOrder {
     public:
     Bomb();
     ~Bomb();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool validate();
 };
 
-class Blockade : public Order {
+/**
+ * Order caused by using a card.
+ **/
+class Blockade : public CardOrder {
     public:
     Blockade();
     ~Blockade();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool validate();
 };
 
-class Airlift : public Order {
+/**
+ * Order caused by using a card.
+ **/
+class Airlift : public CardOrder {
     public:
     Airlift();
     ~Airlift();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool validate();
 };
 
 class Negotiate : public Order {
     public:
     Negotiate();
     ~Negotiate();
-    virtual void execute();
-    virtual bool validate();
+    void execute();
+    bool onCancel();
+    bool validate();
 };
