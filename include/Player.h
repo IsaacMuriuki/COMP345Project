ff1a6126964b8570ecc375269e2249fc93d1859c
Isaac Muriuki <i_muriuk@live.concordia.ca>
2021-10-08 00:47:28 -0400

    more cleanup and commenting

--- include/Player.h
+++ include/Player.h
@@ -12,30 +12,29 @@
 
 class Player{
 public:
-    /*
-     * Constructors, destructor and copy constructor
-     */
+    // Constructors, destructor and copy constructor
     Player();
     Player(string, vector<Territory*> , OrdersList*, Hand* );
     ~Player();
     Player(const Player& player);
 
+    // Methods we were asked to create
     vector<Territory*> toDefend();
     vector<Territory*> toAttack();
     void issueOrder();
 
+    // Helper methods
     void addOrder(Order* );
     void addTerritory(Territory* );
-
     vector<Territory*> getTerritories();
     OrdersList* getOrdersList();
     Hand* getHandOfCards();
     string getName();
-
     void setHandOfCards(Hand* );
     void setTerritories(vector<Territory*> );
     void setOrders(OrdersList* );
 
+    // Overloading << and = operators
     friend std::ostream& operator<<(std::ostream &, const Player& );
     Player& operator=(const Player& player);
 private:
--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -1,5 +1,6 @@
 #include <Player.h>
 
+// Default constructor
 Player::Player() {
     this->name = "";
     this->territories = vector<Territory*>();
@@ -7,6 +8,17 @@ Player::Player() {
     this->handOfCards = new Hand();
 }
 
+/**
+ * Parameterized constructor
+ *
+ * Creates a deep copy of ordersList and handOfCards
+ * Creates a shallow copy of territories as per prof's answer - they are unique and therefore the vector of territories is a shallow copy
+ *
+ * @param name
+ * @param territories
+ * @param ordersList
+ * @param handOfCards
+ */
 Player::Player(string name, vector<Territory*> territories, OrdersList* ordersList, Hand* handOfCards){
     this->name = name;
     this->territories = territories;
@@ -14,6 +26,7 @@ Player::Player(string name, vector<Territory*> territories, OrdersList* ordersLi
     this->handOfCards = new Hand(*handOfCards);
 }
 
+// Destructor
 Player::~Player(){
     delete handOfCards; handOfCards = NULL;
     delete ordersList; ordersList = NULL;
@@ -24,6 +37,11 @@ Player::~Player(){
     }
 }
 
+/**
+ * Copy constructor
+ *
+ * @param player
+ */
 Player::Player(const Player& player){
     this->name = name;
     this->territories = territories;
@@ -31,14 +49,62 @@ Player::Player(const Player& player){
     this->handOfCards = new Hand(*handOfCards);
 }
 
+
+/**
+ * Overloading the assignment operator
+ *
+ * @param player
+ * @return
+ */
+Player& Player::operator=(const Player& player){
+    this->name = player.name;
+    this->territories = player.territories;
+    this->ordersList = new OrdersList(*player.ordersList);
+    this->handOfCards = new Hand(*player.handOfCards);
+
+    return *this;
+}
+
+/**
+ * Overloading stream insertion operator
+ *
+ * @param strm
+ * @param player
+ * @return
+ */
+std::ostream& operator<<(std::ostream &strm, const Player& player){
+
+    strm << "Player Data:\n";
+    strm << "Name :" << player.name << "\n";
+    strm << "Territories \n";
+    for (int i = 0; i < player.territories.size(); ++i) {
+        strm << *player.territories[i];
+    }
+    strm << "Orders \n";
+    for (int i = 0; i < player.ordersList->size(); ++i) {
+        strm << player.ordersList->get(i);
+    }
+    strm << "Cards \n";
+    for (int i =0; i <sizeof(player.handOfCards->hand)/sizeof (player.handOfCards->hand[0]); ++i){
+        strm << player.handOfCards->hand[i].getCardType() << "\n";
+    }
+
+    return strm;
+}
+
+// Asked to return an arbitrary list of territories for this, so I just return all the players territories
 vector<Territory*> Player::toDefend(){ return territories;}
 
+// Asked to return an arbitrary list of territories for this, so I just return all the players territories
 vector<Territory*> Player::toAttack(){ return territories;}
 
+
+// Asks the user for input on which order to create, and creates the corresponding order objects and adds it to the player's ordersList
 void Player::issueOrder() {
     string orders = "List of possible orders to be made: \n 1. Deploy\n 2. Advance\n 3. Bomb\n 4. Blockade\n 5. Airlift\n ^6. Negotiate\n Enter the number of the order you want to execute: ";
     int orderNumber;
 
+    // Asking player for which order and validates choice
     while (true)
     {
         std::cin >> orderNumber;
@@ -111,36 +177,9 @@ void Player::issueOrder() {
     }
 }
 
-std::ostream& operator<<(std::ostream &strm, const Player& player){
-
-    strm << "Player Data:\n";
-    strm << "Name :" << player.name << "\n";
-    strm << "Territories \n";
-    for (int i = 0; i < player.territories.size(); ++i) {
-        strm << *player.territories[i];
-    }
-    strm << "Orders \n";
-    for (int i = 0; i < player.ordersList->size(); ++i) {
-        strm << player.ordersList->get(i);
-    }
-    strm << "Cards \n";
-    for (int i =0; i <sizeof(player.handOfCards->hand)/sizeof (player.handOfCards->hand[0]); ++i){
-        strm << player.handOfCards->hand[i].getCardType() << "\n";
-    }
-
-    return strm;
-}
-
-
-Player& Player::operator=(const Player& player){
-    this->name = player.name;
-    this->territories = player.territories;
-    this->ordersList = new OrdersList(*player.ordersList);
-    this->handOfCards = new Hand(*player.handOfCards);
-
-    return *this;
-}
-
+/**
+ * Setters and Getters
+ */
 void Player::addOrder(Order *order) { ordersList->add(order);}
 
 void Player::addTerritory(Territory *territory) { territories.push_back(territory);}
